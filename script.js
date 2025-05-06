8a662cbc9a68ba5fe43ff0544a086d9a127f058a
Andrew Skow <skovorodnikov11@gmail.com>
2025-05-07 00:16:30 +0300

    lesson9completed

--- script.js
+++ script.js
@@ -1,47 +1,104 @@
 const addCommentButtunElement = document.getElementById("add-comment-button");
-console.log(addCommentButtunElement);
+addCommentButtunElement.classList.add("inactive");
+addCommentButtunElement.disabled = true;
 
-addCommentButtunElement.addEventListener("click", () => {
-  let commentListElemnet = document.getElementById("comment-list");
-  const inputNameElement = document.getElementById("input-name");
-  const inputCommentElement = document.getElementById("input-comment");
-  const currentDate = new Date();
+const inputNameElement = document.getElementById("input-name");
+const inputCommentElement = document.getElementById("input-comment");
+const commentListElemnet = document.getElementById("comment-list");
 
-  inputNameElement.classList.remove("error");
-  inputCommentElement.classList.remove("error");
+// функция проверки заполненности полей
+const checkInputs = () => {
+  const isNameFilled = inputNameElement.value !== "";
+  const isCommentFilled = inputCommentElement.value !== "";
 
-  if (inputNameElement.value == "") {
-    alert("Имя не может быть пустым!");
+  if (isNameFilled == true) {
+    console.log("Какое-то имя введно");
+    inputNameElement.classList.remove("error");
+  } else {
+    console.log("Поле ввода имени пустое");
     inputNameElement.classList.add("error");
-    return;
+    inputNameElement.placeholder = "Имя не может быть пустым";
   }
-  if (inputCommentElement.value == "") {
-    alert("Комментарий не может быть пустым");
+
+  if (isCommentFilled == true) {
+    console.log("Комментарий введён");
+    inputCommentElement.classList.remove("error");
+  } else {
+    console.log("Комментарий пустой");
     inputCommentElement.classList.add("error");
-    return;
+    inputCommentElement.placeholder = "Комментарий не может быть пустым!";
   }
+
+  addCommentButtunElement.disabled = !(isNameFilled && isCommentFilled);
+  if (addCommentButtunElement.disabled) {
+    addCommentButtunElement.classList.add("inactive");
+  } else {
+    addCommentButtunElement.classList.remove("inactive");
+  }
+};
+
+// проверка полей при вводе
+inputNameElement.addEventListener("input", checkInputs);
+inputCommentElement.addEventListener("input", checkInputs);
+
+// функция добавления комментария
+const addComment = () => {
+  const currentDate = new Date();
+  const formattedDateTime = currentDate
+    .toLocaleString("ru-Ru", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+    })
+    .replace(/,/, "");
 
   commentListElemnet.innerHTML += `
-          <li class="comment">
-          <div class="comment-header">
-            <div>${inputNameElement.value}</div>
-            <div>${
-              currentDate.toLocaleDateString("Ru-ru") +
-              " " +
-              currentDate.toLocaleTimeString("ru")
-            }</div>
-          </div>
-          <div class="comment-body">
-            <div class="comment-text">
-            ${inputCommentElement.value}
-            </div>
-          </div>
-          <div class="comment-footer">
-            <div class="likes">
-              <span class="likes-counter">0</span>
-              <button class="like-button -active-like"></button>
-            </div>
-          </div>
-        </li>
-  `;
+        <li class="comment">
+
+    <div class="comment-header">
+      <div>${inputNameElement.value}</div>
+      <div>${formattedDateTime}</div>
+    </div>
+    <div class="comment-body">
+      <div class="comment-text">
+      ${inputCommentElement.value}
+      </div>
+    </div>
+    <div class="comment-footer">
+      <div class="likes">
+      <span class="likes-counter">0</span>
+      <button class="like-button -active-like"></button>
+    </div>
+    </div>
+    </li>
+    `;
+
+  // очищение полей ввода
+  inputNameElement.value = "";
+  inputCommentElement.value = "";
+  addCommentButtunElement.classList.add("inactive");
+};
+
+// вешаем функцию на кнопку
+addCommentButtunElement.addEventListener("click", addComment);
+
+// навешивание клавиши enter
+const handleKeyPress = (e) => {
+  if (e.key == "Enter" && !addCommentButtunElement.disabled) {
+    addComment();
+  }
+};
+
+document.addEventListener("keypress", handleKeyPress);
+
+// likes
+
+let likes = 0;
+const likeButton = document.getElementById("like-button");
+likeButton.addEventListener("click", () => {
+  likes++;
+  console.log("поставлен лайк");
+  document.getElementById("number-likes").innerHTML = `${likes}`;
 });
