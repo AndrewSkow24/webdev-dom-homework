deca30c490774b51ab632060c8ee714e88ee5641
Andrew Skow <skovorodnikov11@gmail.com>
2025-05-13 00:49:50 +0300

    api_version

--- script.js
+++ script.js
@@ -1,243 +1,72 @@
-const addCommentButtunElement = document.getElementById("add-comment-button");
-addCommentButtunElement.classList.add("inactive");
-addCommentButtunElement.disabled = true;
-
+const commentsListElement = document.getElementById("comment-list");
+const addCommentButtonElement = document.getElementById("add-comment-button");
 const inputNameElement = document.getElementById("input-name");
 const inputCommentElement = document.getElementById("input-comment");
-const commentListElemnet = document.getElementById("comment-list");
 
-const deleteButton = document.getElementById("delete-comment-button");
+let commentsArray = [];
 
-const commentsListData = [
-  {
-    name: "Глеб Фокин",
-    formattedDateTime: "12.02.22 12:18",
-    comment: "Это будет первый комментарий на этой странице",
-    likes: 3,
-    isLikeActive: false,
-    isEdit: false,
-  },
-  {
-    name: "Варвара Н.",
-    formattedDateTime: "13.02.22 19:22",
-    comment: "Мне нравится как оформлена эта страница! ❤",
-    likes: 75,
-    isLikeActive: true,
-    isEdit: false,
-  },
-];
-
-// инициализация каждого комментария
-const initCommentEl = () => {
-  const comments = document.querySelectorAll(".comment");
-
-  for (const comment of comments) {
-    comment.addEventListener("click", () => {
-      const index = comment.dataset.index;
-
-      // alert(`Вы нажали на коммент, индекс: ${index}`);
-      inputCommentElement.value =
-        "QUOTE_BEGIN " +
-        commentsListData[index].name +
-        "\n" +
-        commentsListData[index].comment +
-        "\n\n" +
-        "QUOTE_END";
+const getAllComments = () => {
+  const fetchPromise = fetch(
+    "https://wedev-api.sky.pro/api/v1/andrew-skow/comments",
+    {
+      method: "GET",
+    }
+  );
+  fetchPromise.then((response) => {
+    const jsonPromise = response.json();
+    jsonPromise.then((responseData) => {
+      commentsArray = responseData.comments;
+      console.log(commentsArray);
+      renderComments();
     });
-  }
+  });
 };
 
-// инициализация кнопки лайка
-const likeButtonEventListeners = () => {
-  const likeButtons = document.querySelectorAll(".like-button");
+getAllComments();
 
-  for (const likeButton of likeButtons) {
-    likeButton.addEventListener("click", (event) => {
-      currentIndexElement = likeButton.dataset.index;
-      commentsListData[currentIndexElement].isLikeActive =
-        !commentsListData[currentIndexElement].isLikeActive;
-      event.stopPropagation();
-      if (!commentsListData[currentIndexElement].isLikeActive) {
-        commentsListData[currentIndexElement].likes -= 1;
-      } else {
-        commentsListData[currentIndexElement].likes += 1;
-      }
-      renderListComments();
-    });
-  }
-};
-
-const editCommentButtonEventListeners = () => {
-  /*
-  Что нужно сделать:
-  Пользователь должен иметь возможность отредактировать любой уже написанный комментарий.
-  Для этого под каждым комментарием должна появиться кнопка «Редактировать».
-  При клике на кнопку «Редактировать», текст комментария должен замениться полем ввода в формате textarea, а кнопка «Редактировать» должна быть заменена на кнопку «Сохранить».
-  В поле ввода должен быть автоматически подставлен текущий текст комментария для удобного редактирования.
-  Пользователь может внести изменения в текст комментария, используя поле ввода.
-  При клике на кнопку «Сохранить» введённые изменения должны быть сохранены в массив данных, а интерфейс должен вернуться в исходное состояние.
-*/
-
-  const editCommentButtons = document.querySelectorAll(".edit-comment-button");
-
-  for (const editCommentButton of editCommentButtons) {
-    editCommentButton.addEventListener("click", (event) => {
-      const index = editCommentButton.dataset.index;
-      alert("Редактирование");
-      const commentText = document.querySelector(
-        `.comment-text[data-index="${index}"]`
-      );
-
-      commentsListData[index].isEdit = true;
-      renderListComments();
-
-      event.stopPropagation();
-    });
-  }
-};
-
-// функция замены текста коммента на поле ввода
-// и кнопки редактировать на сохранить
-
-// функция отображения элементов массива
-const renderListComments = () => {
-  const commentsHtml = commentsListData
-    .map((comment, index) => {
-      // console.log(comment);
+const renderComments = () => {
+  const commentsHtml = commentsArray
+    .map((comment) => {
       return `
-      <li class="comment" data-index="${index}">
+     <li class="comment data-id=${comment.id}">
           <div class="comment-header">
-            <div>${comment.name}</div>
-            <div>${comment.formattedDateTime}</div>
+            <div>${comment.author.name}</div>
+            <div>${comment.date}</div>
           </div>
           <div class="comment-body">
-          ${
-            comment.isEdit == false
-              ? `<div class="comment-text" data-index="${index}">
-            ${comment.comment
-              .replaceAll("QUOTE_BEGIN", "<div class='quote'>")
-              .replaceAll("QUOTE_END", "</div>")}
-            </div>`
-              : `<textarea></textarea>`
-          }
+            <div class="comment-text">
+            ${comment.text}
+            </div>
           </div>
           <div class="comment-footer">
             <div class="likes">
               <span class="likes-counter">${comment.likes}</span>
-              <button data-index="${index}" class="like-button ${
-        comment.isLikeActive ? "-active-like" : ""
-      }"></button>
+              <button class="like-button"></button>
             </div>
           </div>
-          ${
-            comment.isEdit === false
-              ? `
-              <button data-index="${index}" class="edit-comment-button">
-                Редактировать
-              </button>
-            `
-              : `
-              <button data-index="${index}" class="edit-comment-button">
-                Сохранить
-              </button>
-            `
-          }
         </li>
-    `;
+      `;
     })
     .join("");
 
-  commentListElemnet.innerHTML = commentsHtml;
-  initCommentEl();
-  likeButtonEventListeners();
-  editCommentButtonEventListeners();
+  commentsListElement.innerHTML = commentsHtml;
 };
 
-renderListComments();
-
-// функция проверки заполненности полей
-const checkInputs = () => {
-  const isNameFilled = inputNameElement.value !== "";
-  const isCommentFilled = inputCommentElement.value !== "";
-
-  if (isNameFilled == true) {
-    console.log("Какое-то имя введно");
-    inputNameElement.classList.remove("error");
-  } else {
-    console.log("Поле ввода имени пустое");
-    inputNameElement.classList.add("error");
-    inputNameElement.placeholder = "Имя не может быть пустым";
-  }
-
-  if (isCommentFilled == true) {
-    console.log("Комментарий введён");
-    inputCommentElement.classList.remove("error");
-  } else {
-    console.log("Комментарий пустой");
-    inputCommentElement.classList.add("error");
-    inputCommentElement.placeholder = "Комментарий не может быть пустым!";
-  }
-
-  addCommentButtunElement.disabled = !(isNameFilled && isCommentFilled);
-  if (addCommentButtunElement.disabled) {
-    addCommentButtunElement.classList.add("inactive");
-  } else {
-    addCommentButtunElement.classList.remove("inactive");
-  }
-};
-
-// проверка полей при вводе
-inputNameElement.addEventListener("input", checkInputs);
-inputCommentElement.addEventListener("input", checkInputs);
-
-// функция добавления комментария
-const addComment = () => {
-  const currentDate = new Date();
-  const formattedDateTime = currentDate
-    .toLocaleString("ru-Ru", {
-      day: "2-digit",
-      month: "2-digit",
-      year: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-    })
-    .replace(/,/, "");
-
-  commentsListData.push({
-    name: inputNameElement.value
-      .replaceAll("<", "&lt;")
-      .replaceAll(">", "&gt;"),
-    formattedDateTime: formattedDateTime,
-    comment: inputCommentElement.value
-      .replaceAll("<", "&lt;")
-      .replaceAll(">", "&gt;"),
-    likes: 0,
-    isLikeActive: false,
+addCommentButtonElement.addEventListener("click", () => {
+  fetch("https://wedev-api.sky.pro/api/v1/andrew-skow/comments", {
+    method: "POST",
+    body: JSON.stringify({
+      text: inputCommentElement.value,
+      name: inputNameElement.value,
+    }),
+  }).then((response) => {
+    response.json().then((responseData) => {
+      console.log(responseData);
+      getAllComments();
+    });
   });
 
-  renderListComments();
-
-  // очищение полей ввода
+  // очистка формы
   inputNameElement.value = "";
   inputCommentElement.value = "";
-  addCommentButtunElement.classList.add("inactive");
-};
-
-// вешаем функцию на кнопку
-addCommentButtunElement.addEventListener("click", addComment);
-
-// навешивание клавиши enter
-const handleKeyPress = (e) => {
-  if (e.key == "Enter" && !addCommentButtunElement.disabled) {
-    addComment();
-  }
-};
-
-document.addEventListener("keypress", handleKeyPress);
-
-// функция удаления последнего комментария
-
-deleteButton.addEventListener("click", () => {
-  commentsListData.pop();
-  renderListComments();
 });
